973c053abcfe04cf9686c0b1976007730510a279
WangZeGuang <1292815828@qq.com>
2017-11-17 17:19:15 +0800

    高级查询完成了

--- src/main/java/com/wzg/ssh/staff/dao/impl/StaffDaoImpl.java
+++ src/main/java/com/wzg/ssh/staff/dao/impl/StaffDaoImpl.java
@@ -1,11 +1,15 @@
 package com.wzg.ssh.staff.dao.impl;
 
+import com.opensymphony.xwork2.ActionContext;
+import com.wzg.ssh.department.domain.Department;
+import com.wzg.ssh.post.domain.Post;
 import com.wzg.ssh.staff.dao.StaffDao;
-import com.wzg.ssh.staff.domain.Department;
-import com.wzg.ssh.staff.domain.Post;
 import com.wzg.ssh.staff.domain.Staff;
+import com.wzg.ssh.utils.MD5Util;
+import org.apache.struts2.ServletActionContext;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
+import javax.servlet.ServletContext;
 import java.util.List;
 
 /**
@@ -13,67 +17,81 @@ import java.util.List;
  */
 public class StaffDaoImpl extends HibernateDaoSupport implements StaffDao {
 
-    // 查处数据库中的员工信息集合
+    // 查查出数据库中的员工信息集合
     @Override
     public Staff login(Staff staff) {
-
+        String md5 = MD5Util.getMd5(staff.getLoginPwd());
+        staff.setLoginPwd(md5);
         List<Staff> staffs = getHibernateTemplate().findByExample(staff);
-
-        if (staffs!=null&&staffs.size() > 0){
+        if (staffs != null && staffs.size() > 0) {
             return staffs.get(0);
+        } else {
+
+            return null;
+        }
+
+    }
+
+    //查询所有员工
+    @Override
+    public List<Staff> getStaff(Staff staff) {
+
+        List<Staff> staffs = getHibernateTemplate().loadAll(Staff.class);
+        if (staffs != null && staffs.size() > 0) {
+            return staffs;
+        }
+        return null;
+    }
+
+
+    // 保存或者编辑员工信息
+    @Override
+    public void saveOrUpdate(Staff staff) {
+        getHibernateTemplate().saveOrUpdate(staff);
+
+    }
+
+    // 查询所有部门
+    @Override
+    public List<Department> getDept() {
+        String sql = "from Department crm_dept";
+        return (List<Department>) getHibernateTemplate().find(sql);
+    }
+
+    @Override
+    public List<Staff> getAllResult(int deptId, int postId, String staffName) {
+
+        if (deptId > 0 && postId == 0 && staffName.equals("")) {
+
+            String sql1 = "from Staff crm_staff where dept.deptId=?";
+            return (List<Staff>) getHibernateTemplate().find(sql1, deptId);
+
+        } else if (deptId == 0 && postId == 0 && !staffName.equals("")) {
+            String sql4 = "from Staff crm_staff where staffName like ?";
+            return (List<Staff>) getHibernateTemplate().find(sql4, "%" + staffName + "%");
+
+
+        } else if (deptId > 0 && postId > 0 && !staffName.equals("")) {
+
+            String sql3 = "from Staff crm_staff where dept.deptId=? and post.postId=? and staffName like ?";
+            return (List<Staff>) getHibernateTemplate().find(sql3, deptId, postId, "%" + staffName + "%");
+
+        } else if (deptId > 0 && postId > 0 && staffName.equals("")) {
+            String sql2 = "from Staff crm_staff where dept.deptId=? and post.postId = ?";
+            return (List<Staff>) getHibernateTemplate().find(sql2, deptId, postId);
+
+        } else if (deptId > 0 && postId == 0 && !staffName.equals("")) {
+
+            String sql6 = "from Staff crm_staff where dept.deptId=? and staffName like ?";
+            getHibernateTemplate().find(sql6, deptId, "%" + staffName + "%");
+        } else {
+
+            String sql5 = "from Staff crm_staff";
+            return (List<Staff>) getHibernateTemplate().find(sql5);
         }
         return null;
 
     }
-
-    // 插入部门的方法
-    @Override
-    public Department saveDept(Department dept) {
-        getHibernateTemplate().save(dept);
-        return dept;
-
-    }
-
-    // 保存员工的方法
-    @Override
-    public Staff saveStaff(Staff staff) {
-        getHibernateTemplate().save(staff);
-        return staff;
-    }
-
-    // 查出所有的部门
-    @Override
-    public List<Department> getDepartment() {
-        return null;
-    }
-
-    // 根据部门id查询所有的职位
-
-    @Override
-    public List<Post> getPostByDeptId(int deptId) {
-        return null;
-    }
-
-    // 根据部门和职位ID查询所有员工
-
-    @Override
-    public List<Staff> getStaffByDeptIdAndPostId(int deptId, int postId) {
-        return null;
-    }
-
-    // 根据部门ID查询所有员工
-
-    @Override
-    public List<Staff> getStaffByDeptId(int deptId) {
-        return null;
-    }
-
-    // 查询所有的员工
-    @Override
-    public List<Staff> getStaffs() {
-        return null;
-    }
-
 
 
 }
